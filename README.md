e6fb2569b4620777025ff635c50a06320ba97e48
abhik <abhiknvarma@gmail.com>
2021-06-14 23:39:19 +0530

    added documentation

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # open_csv_webapp
+## Instructions
+Steps to open web app:
+```
+git clone https://github.com/abhikvarma/open_csv_webapp.git
+cd open_csv_webapp
+pip install streamlit
+streamlit run open_csv.py
+```
+
+Use `ctrl+c` to stop execution  
+Variable `dir` points to the folder containing the CSV files
+
--- open_csv.py
+++ open_csv.py
@@ -3,19 +3,25 @@ import os
 import pandas as pd
 
 #assuming all the csv files are stored in the 'csvs' folder
-csv_list = os.listdir("csvs")
+dir = "csvs"
+csv_list = os.listdir(dir)
 
 st.title("CSV Scanner")
 
+#the selected file name is returned 
 file = st.selectbox("Select a file from the dropdown", options = csv_list)
 
+#the csv file is read as a pandas dataframe and
+#a list of column names are stored
 csv_df = pd.read_csv(open("csvs/"+file))
 cols = list(csv_df.columns)
 
+#the delected field are stored in a list
 fields = st.multiselect('Select required fields from the file', options = cols)
 
 submit = st.button("Submit")
 
+#displays the selected fields from the file
 if submit:
     st.write("  \n")
     st.write("Selected file and fields:")
